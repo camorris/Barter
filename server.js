8e26ec17b91a93e8dba25b029a8f4ab9fe3565ee
Chase Morris <camorris@callutheran.edu>
2017-11-05 21:33:19 -0800

    made server file

---
+++ server.js
@@ -0,0 +1,25 @@
+const
+  express = require('express'),
+  app = express(),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/project_4',
+  PORT = process.env.PORT || 3000,
+  usersRoutes = require('./routes/users.js')
+
+mongoose.connect(MONGODB_URI, (err)=>{
+  console.log(err|| `Connected to MongoDB.`)
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/api',(req,res)=>{
+  res.json({message: "API root."})
+})
+
+app.use('/api/users', usersRoutes)
+ app.listen(PORT, (err)=>{
+   console.log(err || `Server running on port ${PORT}`)
+ })
