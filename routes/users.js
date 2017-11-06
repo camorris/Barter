3e9c10cfd6246edf281d10b257aaf8f432031810
Chase Morris <camorris@callutheran.edu>
2017-11-06 10:18:57 -0800

    made .env, gitignore and updated routes file

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const
+  express = require('express')
+  userRouter = new express.Router(),
+  usersCtrl = require('../controller/users.js'),
+  verifyToken = require('../serverAuth.js').verifyToken
+
+  usersRouter.route('/')
+    .get(usersCtrl.index)
+    .post(usersCtrl.create)
+
+  usersRouter.post('authenticate', userCtrl.authenticate)
+
+  usersRouter.use(verifyToken)
+  usersRouter.route('/:id')
+    .get(usersCtrl.show)
+    .patch(usersCtrl.update)
+    .delete(usersCtrl.destroy)
+
+  module.exports
