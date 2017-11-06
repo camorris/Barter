07ee88b2e3f878c18e94d2b0bb61b256b727c01f
Ryan Culton <rculton@gmail.com>
2017-11-06 11:56:32 -0800

    Added a post model

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+const
+    mongoose = require('mongoose')
+    postSchema = new mongoose.Schema({
+        title: {type: String, required: true},
+        body: {type: String},
+        item: {type: String, required: true},
+        exchangeFor: {type: String, required: true},
+        location: {type: String, required: true},
+        userId: {type: String, required: true},
+        image: {type: String},
+        cashValue: {type: Number}
+    })
+
+//
+
+const Post = mongoose.model('Post', postSchema)
+module.exports = User
