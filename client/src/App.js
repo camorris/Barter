50464bbc9a2186c4efa5e939a6b85e154ac50aa4
Chase Morris <camorris@callutheran.edu>
2017-11-06 17:09:33 -0800

    setup react files

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+// import clientAuth from './clientAuth'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import NavBar from './NavBar'
+import LogIn from './views/LogIn'
+import LogOut from './views/LogOut'
+import SignUp from './views/SignUp'
+import VIP from './views/VIP'
+import Home from './views/Home'
+
+class App extends React.Component {
+	state = {currentUser: null}
+	
+	componentDidMount() {
+		this.setState({currentUser: clientAuth.getCurrentUser()})
+	}
+	
+	onLoginSuccess(user){
+		this.setState({currentUser: clientAuth.getCurrentUser()})
+	}
+
+	onSignUpSuccess(user){
+		this.setState({currentUser: clientAuth.getCurrentUser()})
+	}
+
+	logOut() {
+		clientAuth.logOut()
+		this.setState({currentUser: null})
+	}
+	render() {
+		console.log(this.state)
+		const { currentUser } = this.state
+ 		return (
+			<div className='App'>
+				<NavBar />
+				<Switch>
+					<Route path="/login" render={(props)=> {
+						return <LogIn {...props} onLoginSuccess={this.onLoginSuccess.bind(this)}/>
+					}} />
+					<Route path="/logout" render={(props) => { 
+						return <LogOut onLogOut={this.logOut.bind(this)}/>
+					}} />
+					<Route path="/signup" render={(props)=> {
+						return <SignUp {...props} onSignUpSuccess={this.onSignUpSuccess.bind(this)}/>
+					}} />
+					<Route path="/vip" render={(props)	 => {
+						return currentUser 
+						? <VIP />
+						: <Redirect to="/login"/>
+					}} />
+					<Route path="/" component={Home} />
+				</Switch>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
